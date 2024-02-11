e11bee6d32b6ae76a5eafcc384f873078535b467
kimsengthai <kimseng.thai@hotmail.com>
2024-02-11 16:21:30 +1100

    still not finish, need to add mongo for data

--- app.js
+++ app.js
@@ -1,55 +1,84 @@
 import express from "express";
 import bodyParser from "body-parser";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
+const currentDate = new Date().toDateString();
+const aboutMe = "I make a good blog post everyday, so make you you read all my posts and make me feel good"
+const contactMe = "Contact me at 3939393939393939 for a discussion about reading blog and how to write one"
 
 var urlencodedParser = bodyParser.urlencoded({ extended: true })
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));	
 
+// routing
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  const blogData = loadExistingData();
+
+  res.render("home.ejs", {blogData: blogData});
 });
 
 app.get("/about", (req, res) => {
-  res.render("about.ejs");
+  res.render("about.ejs", {about: aboutMe});
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
-});
-
-app.get("/composer", (req, res) => {
-  res.render("composer.ejs");
+  res.render("contact.ejs", {contact: contactMe});
 });
 
 app.get("/post", (req, res) => {
   res.render("post.ejs");
 });
 
-app.post("/submit", (req, res) => {
-  const blogTitleUser = req.body.blogTitleInput;
-  const blogBodyUser = req.body.blogBodyInput;
+app.get("/composer", (req, res) => {
+  const blogData = loadExistingData();
 
-  // Render composer.ejs and pass data
-  res.render("composer.ejs", {
-    blogTitle: blogTitleUser,
-    blogBody: blogBodyUser
-  });
+  res.render("composer.ejs", {blogData: blogData});
 });
 
-// Define another route for rendering home.ejs
+// function to save file to JSON file
+function saveDataToFile(blogData) {
+  const jsonData = JSON.stringify(blogData);
+  fs.writeFile('blogData.json', jsonData, (err) => {
+    if (err) {
+      console.error('Error saving data to file:', err);
+    } else {
+      console.log('Data saved to file');
+    }
+  });
+}
+
+// function to load existing data, inorder to transfer data and not replace
+function loadExistingData() {
+  try {
+    const data = fs.readFileSync('blogData.json', 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading data from file:', error);
+    return [];
+  }
+}
+
+// logic read file first then save to json
 app.post("/submit", (req, res) => {
   const blogTitleUser = req.body.blogTitleInput;
   const blogBodyUser = req.body.blogBodyInput;
 
-  // Render composer.ejs and pass data
-  res.render("composer.ejs", {
+  const data = {
     blogTitle: blogTitleUser,
-    blogBody: blogBodyUser
-  });
+    blogBody: blogBodyUser,
+    today: currentDate
+  };
+
+  let blogData = loadExistingData();
+
+  blogData.push(data);
+
+  saveDataToFile(blogData);
+
+  res.render("/", data);
 });
 
 app.listen(port, () => {
